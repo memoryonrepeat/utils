d509c3914ca7e39a02022ed1bb3edf0090350ea6
Thanh Nguyen <tienthanh8490@gmail.com>
2016-10-17 15:16:24 +0800

    add timer

---
+++ timer/timer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Command line reminder that runs in background and notify using OS X notification center
+# Usage: remind <minutes> [hours] [message]
+minutes=${1:-1}; # default = 1
+hours=${2:-0}; # default = 0
+text=${3:-"Task"}; # default = "Task"
+echo "Time's up after $hours hours $minutes minutes"
+let secs=$minutes*1+$hours*60*60
+sleep $secs
+osascript -e "display notification \"$text\" with title \"Reminder\""
