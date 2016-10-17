1f8854f79411ed37107032900d21031a06431560
Thanh Nguyen <tienthanh8490@gmail.com>
2016-10-17 15:48:33 +0800

    add missing param

--- timer/timer.sh
+++ timer/timer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Command line reminder that runs in background and notify using OS X notification center
+# Add the below function to .bashrc or .zshrc before using
 # Usage: remind <minutes> [hours] [message]
 minutes=${1:-1}; # default = 1
 hours=${2:-0}; # default = 0
@@ -8,3 +9,9 @@ echo "Time's up after $hours hours $minutes minutes"
 let secs=$minutes*60+$hours*60*60
 sleep $secs
 osascript -e "display notification \"$text\" with title \"Reminder\""
+
+# https://stackoverflow.com/questions/7131670/make-bash-alias-that-takes-parameter
+# https://stackoverflow.com/questions/7131670/make-bash-alias-that-takes-parameter#comment36589234_7131683
+# remind(){
+# 	. ~/timer.sh $1 $2 $3 &
+# }
