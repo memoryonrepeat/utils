38fec45a3f29bd340c03f35eaec771fad5656769
Thanh Nguyen <tienthanh8490@gmail.com>
2016-10-17 15:36:38 +0800

    change constant

--- timer/timer.sh
+++ timer/timer.sh
@@ -5,6 +5,6 @@ minutes=${1:-1}; # default = 1
 hours=${2:-0}; # default = 0
 text=${3:-"Task"}; # default = "Task"
 echo "Time's up after $hours hours $minutes minutes"
-let secs=$minutes*1+$hours*60*60
+let secs=$minutes*60+$hours*60*60
 sleep $secs
 osascript -e "display notification \"$text\" with title \"Reminder\""
